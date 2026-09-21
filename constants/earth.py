9de150d4bd4e994b08836cf4bf6e6d39f66bc039
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2026-09-20 20:58:23 -0400

    Simple sphere

---
+++ constants/earth.py
@@ -0,0 +1,23 @@
+"""
+Constants relating to Earth.
+"""
+
+mass = 5.972e24
+"""
+float: Mass of the Earth (kg)
+"""
+
+radius = 6378.1363
+"""
+float: Radius of the Earth (km)
+"""
+
+mu = 398600.4415
+"""
+float: Standard gravitation constant (km^3 / s^2). G * M
+"""
+
+seconds = 806.81099130673
+"""
+float: 1.0 TU (solar s)
+"""
